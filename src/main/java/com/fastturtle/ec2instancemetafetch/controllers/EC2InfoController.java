b90aa913769a2cf5e91849bdad1f463733b22db9
divya0319 <divygupta0319@gmail.com>
2024-12-05 12:27:10 +0530

    Commented out EC2 instance name fetching code, alongwith ec2 dependency

--- src/main/java/com/fastturtle/ec2instancemetafetch/controllers/EC2InfoController.java
+++ src/main/java/com/fastturtle/ec2instancemetafetch/controllers/EC2InfoController.java
@@ -3,9 +3,9 @@ package com.fastturtle.ec2instancemetafetch.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import software.amazon.awssdk.services.ec2.Ec2Client;
-import software.amazon.awssdk.services.ec2.model.DescribeInstancesRequest;
-import software.amazon.awssdk.services.ec2.model.DescribeInstancesResponse;
+//import software.amazon.awssdk.services.ec2.Ec2Client;
+//import software.amazon.awssdk.services.ec2.model.DescribeInstancesRequest;
+//import software.amazon.awssdk.services.ec2.model.DescribeInstancesResponse;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -30,13 +30,13 @@ public class EC2InfoController {
             // Get the availability zone
             String availabilityZone = getMetadata("placement/availability-zone");
 
-            try (Ec2Client ec2Client = Ec2Client.create()) {
-                String instanceName = getInstanceName(ec2Client, instanceId);
-
-                response.put("instanceName", instanceName);
-            } catch(Exception e) {
-                response.put("error1", e.getMessage());
-            }
+//            try (Ec2Client ec2Client = Ec2Client.create()) {
+//                String instanceName = getInstanceName(ec2Client, instanceId);
+//
+//                response.put("instanceName", instanceName);
+//            } catch(Exception e) {
+//                response.put("error1", e.getMessage());
+//            }
 
             // Output the details
 
@@ -66,23 +66,23 @@ public class EC2InfoController {
         }
     }
 
-    private static String getInstanceName(Ec2Client ec2Client, String instanceId) {
-        // Describe instance to fetch its tags
-        DescribeInstancesRequest request = DescribeInstancesRequest.builder()
-                .instanceIds(instanceId)
-                .build();
-        DescribeInstancesResponse response = ec2Client.describeInstances(request);
-
-        // Extract the "Name" tag from the instance details
-        return response.reservations().stream()
-                .flatMap(reservation -> reservation.instances().stream())
-                .filter(instance -> instance.instanceId().equals(instanceId))
-                .flatMap(instance -> instance.tags().stream())
-                .filter(tag -> tag.key().equals("Name"))
-                .map(tag -> tag.value())
-                .findFirst()
-                .orElse("No Name Tag Found");
-    }
+//    private static String getInstanceName(Ec2Client ec2Client, String instanceId) {
+//        // Describe instance to fetch its tags
+//        DescribeInstancesRequest request = DescribeInstancesRequest.builder()
+//                .instanceIds(instanceId)
+//                .build();
+//        DescribeInstancesResponse response = ec2Client.describeInstances(request);
+//
+//        // Extract the "Name" tag from the instance details
+//        return response.reservations().stream()
+//                .flatMap(reservation -> reservation.instances().stream())
+//                .filter(instance -> instance.instanceId().equals(instanceId))
+//                .flatMap(instance -> instance.tags().stream())
+//                .filter(tag -> tag.key().equals("Name"))
+//                .map(tag -> tag.value())
+//                .findFirst()
+//                .orElse("No Name Tag Found");
+//    }
 
 
 }
